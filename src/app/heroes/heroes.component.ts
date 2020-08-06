a90004ae5719b7032821ededc96b3bd0340ef192
Pascal Fossouo <kenfosso@gmail.com>
2020-08-06 00:39:26 -0300

    Update

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {Hero} from '../hero';
-import {MessageService} from '../message.service';
-import {HeroService} from '../hero.service';
+
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,24 +9,23 @@ import {HeroService} from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
   heroes: Hero[];
 
-  constructor(private heroService:HeroService, private messageService:MessageService) { }
+  constructor(private heroService: HeroService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.getHeroes();
   }
 
-  getHeroes(): void{
+  getHeroes(): void {
     this.heroService.getHeroes()
     .subscribe(heroes => this.heroes = heroes);
   }
 
-  add(name:string): void{
+  add(name: string): void {
     name = name.trim();
-    if(!name){return;}
-    this.heroService.addHero({name} as Hero)
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
       .subscribe(hero => {
         this.heroes.push(hero);
       });
